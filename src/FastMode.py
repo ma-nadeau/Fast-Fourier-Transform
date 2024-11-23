00f96ea65ec66c05b5c420697f8d8352de34d2eb
Kalamar136 <karl.bridi@mail.mcgill.ca>
2024-11-23 13:13:53 -0500

    Adding function to compare custom FFT with numpy FFT

--- src/FastMode.py
+++ src/FastMode.py
@@ -37,3 +37,42 @@ def plot_fast_mode(
         plt.savefig(os.path.join(folder_path, f"{image_name}_FastMode_FFT.png"))
 
     plt.show()
+
+def plot_testing_fast_mode(
+    original_image: np.ndarray, image_name: str, save_plot: bool = False
+) -> None:
+    """Plot the original image and its Fourier transform."""
+
+    # First scale the image
+    scaled_image = rescale_image_power2(original_image)
+
+    plt.figure(figsize=(10, 6))
+    plt.suptitle("Fast Fourier Transform Analysis", fontsize=18, fontweight="bold")
+
+    # Plot original image
+    plt.subplot(1, 3, 1)
+    plt.imshow(scaled_image, cmap="gray")
+    plt.title("Scaled Original Image", fontsize=14, fontweight="bold")
+    plt.axis("off")
+
+    # Plot Custom FFT
+    plt.subplot(1, 3, 2)
+    plt.imshow(np.abs(transform_2D(scaled_image)), cmap="gray", norm=LogNorm())
+    plt.title("FFT (Log Scale)", fontsize=14, fontweight="bold")
+    plt.axis("off")
+
+    # Plot Numpy FFT
+    plt.subplot(1, 3, 3)
+    plt.imshow(np.abs(np.fft.fft2(scaled_image)), cmap="gray", norm=LogNorm())
+    plt.title("Numpy FFT (Log Scale)", fontsize=14, fontweight="bold")
+    plt.axis("off")
+
+    plt.tight_layout()
+
+    if save_plot:
+        folder_path = "../Results"
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        plt.savefig(os.path.join(folder_path, f"{image_name}_Testing_FastMode_FFT.png"))
+
+    plt.show()
